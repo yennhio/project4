99b3641804f1ba7c6c5b725bb7f3720561ccbf76
Yennhi Dang <nhidangn@gmail.com>
2021-04-06 16:16:23 -0700

    file reader

--- MaxHeapClient.java
+++ MaxHeapClient.java
@@ -1,5 +1,24 @@
 import java.util.Arrays;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 
 public class MaxHeapClient<T> {
-    
+  public static void main(String[] args) {
+    try {
+        unsorted = []
+      File myObj = new File("test.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+
+  }
 }
