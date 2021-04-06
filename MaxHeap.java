7ebffc539e08c73de001cccd5c4f2842e84017d5
Yennhi Dang <nhidangn@gmail.com>
2021-04-06 13:24:35 -0700

    constructors

--- MaxHeap.java
+++ MaxHeap.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+import javax.naming.ldap.InitialLdapContext;
+
 public class MaxHeap<T extends Comparable<? super T>>
                                 implements MaxHeapInterface<T> 
 {
@@ -8,6 +10,23 @@ public class MaxHeap<T extends Comparable<? super T>>
     private boolean initialized = false;
     private static final int DEFAULT_CAPACITY = 25;
     private static final int MAX_CAPACITY = 10000;
+
+    public MaxHeap() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public MaxHeap(int initialCapacity) {
+        if (initialCapacity < DEFAULT_CAPACITY)
+            initialCapacity = DEFAULT_CAPACITY;
+        else
+            checkCapacity(initialCapacity);
+        
+        @SuppressWarnings("unchecked")
+        T[] tempHeap = (T[]) new Comparable[initialCapacity + 1];
+        heap = tempHeap;
+        lastIndex = 0;
+        initialized = true;
+    }
 
     public T getMax() {
         checkInitialization();
