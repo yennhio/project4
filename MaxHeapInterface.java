697c0f030840040fec1928e5eea29eaf1be30004
Yennhi Dang <nhidangn@gmail.com>
2021-04-06 12:53:41 -0700

    interface: initial methods

--- MaxHeapInterface.java
+++ MaxHeapInterface.java
@@ -1,3 +1,11 @@
 
 
-public inter
+public interface MaxHeapInterface<T>
+{
+    public void add(T newEntry);
+    public T removeMax();
+    public T getMax();
+    public boolean isEmpty();
+    public int getSize();
+    public void clear();
+}
