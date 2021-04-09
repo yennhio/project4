76a7ab88cc42a46936e0173348b6094e8fc8a945
Yennhi Dang <nhidangn@gmail.com>
2021-04-09 13:59:50 -0700

    added FileWriter

--- MaxHeapClient.java
+++ MaxHeapClient.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner; // Import the Scanner class to read text files
-
-import org.junit.Test;
 
 public class MaxHeapClient<T> {
   public static void main(String[] args) {
@@ -26,17 +26,36 @@ public class MaxHeapClient<T> {
 
     System.out.println(Arrays.toString(original));
     
-    MaxHeapInterface<Integer> test = new MaxHeap<>();
-    for (int j=0; j<original.length;j++)
-      test.add(original[j]);
-    
-    int[] heaped = new int[6];
-    heaped[0] = test;
-    
-    System.out.println(heaped);
-    
-    
+    try {
+      File myObj = new File("answer.txt");
+      if (myObj.createNewFile()) {
+        System.out.println("File created: " + myObj.getName());
+      } else {
+        System.out.println("File already exists.");
+      }
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
 
+    try {
+      FileWriter myWriter = new FileWriter("answer.txt");
+      myWriter.write("hello");
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+
+    
+    MaxHeapInterface<Integer> first = new MaxHeap<Integer>();
+    for (int j=0; j<original.length;j++)
+      first.add(original[j]);
+
+    first.removeMax();
+
+    System.out.println(first); 
   }
 
 }
