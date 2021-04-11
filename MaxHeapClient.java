7976245670367c2197e210694e14624e29ca95c1
Yennhi Dang <nhidangn@gmail.com>
2021-04-10 19:00:38 -0700

    sequential adding successful

--- MaxHeapClient.java
+++ MaxHeapClient.java
@@ -39,6 +39,41 @@ public class MaxHeapClient<T> {
     String sequential2;
     sequential2 = "Heap after 10 removals: " + first.toArray();
 
+    int[] original2 = new int[100];
+    int k = 0;
+    try {
+      File myObj2 = new File("data_random.txt");
+      Scanner myReader2 = new Scanner(myObj2);
+      while (myReader2.hasNextInt()) {
+        int data2 = myReader2.nextInt();
+        original2[k] = data2;
+        k++;
+      }
+
+      myReader2.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+
+    MaxHeapInterface<Integer> second = new MaxHeap<Integer>();
+    for (int u=0; u<original2.length;u++)
+      second.add(original2[u]);
+    
+    System.out.println(Arrays.toString(original2));
+    System.out.println(second.toArray());
+  
+    String sequential3;
+    sequential3 = "Heap built using sequential insertions: " + second.toArray();
+
+    String count2 = second.keepCount();
+
+    for (int j=0; j<10;j++)
+      second.removeMax();
+
+    String sequential4;
+    sequential4 = "Heap after 10 removals: " + second.toArray();
+
     try {
       File myObj = new File("answer.txt");
       if (myObj.createNewFile()) {
@@ -53,7 +88,8 @@ public class MaxHeapClient<T> {
 
     try {
       FileWriter myWriter = new FileWriter("answer.txt");
-      myWriter.write(sequential1 + "\n" + count + "\n" + sequential2);
+      myWriter.write(sequential1 + "\n" + count + "\n" + sequential2 + "\n\n");
+      myWriter.write(sequential3 + "\n" + count2 + "\n" + sequential4);
       myWriter.close();
       System.out.println("Successfully wrote to the file.");
     } catch (IOException e) {
